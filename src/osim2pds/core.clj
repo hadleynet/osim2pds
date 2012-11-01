6f33d20bed93a6bbea246f4e338b35bd61ebec2c
Marc Hadley <mhadley@mitre.org>
2012-11-01 17:01:03 -0400

    Factor OSIM-specific code into separate file

--- src/osim2pds/core.clj
+++ src/osim2pds/core.clj
@@ -1,54 +1,6 @@
 (ns osim2pds.core
-  (:gen-class))
-
-(use 'korma.db)
-(use 'korma.core)
-
-(defdb prod ( oracle {
-  :subname "@//stagevpc27.mitre.org:1521/ora1"
-  :user "osim_cdm"
-  :password "osim_cdm"}
-))
-  
-(defentity person
-  (pk :PERSON_ID)
-  (table :PERSON)
-)  
-
-(defentity medication
-  (pk :DRUG_ERA_ID)
-  (table :DRUG_ERA)
-  (belongs-to person {:fk :PERSON_ID})
-)
-
-(defentity condition
-  (pk :CONDITION_ERA_ID)
-  (table :CONDITION_ERA)
-  (belongs-to person {:fk :PERSON_ID})
-)
-
-(defn osim_person
-  "Lookup a patient by id"
-  [id]
-  (select person
-    (where {:PERSON_ID id})
-  )
-)
-
-(defn osim_person_medications
-  "Lookup a patients medications by patient_id"
-  [id]
-  (select medication
-    (where {:PERSON_ID id})
-  )
-)
-
-(defn osim_person_conditions
-  "Lookup a patients conditions by patient_id"
-  [id]
-  (select condition
-    (where {:PERSON_ID id})
-  )
+  (:gen-class)
+  (:use osim2pds.osim)
 )
 
 (defn -main
